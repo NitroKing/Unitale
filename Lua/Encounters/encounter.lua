727e1444353f10e4d58ea8574317a6f1fe9b148a
NitroKing <quin.john99@gmail.com>
2016-02-13 09:34:30 -0600

    Added for snowman

--- Lua/Encounters/encounter.lua
+++ Lua/Encounters/encounter.lua
@@ -1,13 +1,13 @@
 -- A basic encounter script skeleton you can copy and modify for your own creations.
 
 -- music = "shine_on_you_crazy_diamond" --Always OGG. Extension is added automatically. Remove the first two lines for custom music.
-encountertext = "Poseur strikes a pose!" --Modify as necessary. It will only be read out in the action select screen.
+encountertext = "You feel cold" --Modify as necessary. It will only be read out in the action select screen.
 nextwaves = {"bullettest_chaserorb"}
 wavetimer = 4.0
 arenasize = {155, 130}
 
 enemies = {
-"poseur"
+"snowman"
 }
 
 enemypositions = {
@@ -15,7 +15,7 @@ enemypositions = {
 }
 
 -- A custom list with attacks to choose from. Actual selection happens in EnemyDialogueEnding(). Put here in case you want to use it.
-possible_attacks = {"bullettest_bouncy", "bullettest_chaserorb", "bullettest_touhou"}
+possible_attacks = {}
 
 function EncounterStarting()
     -- If you want to change the game state immediately, this is the place.
